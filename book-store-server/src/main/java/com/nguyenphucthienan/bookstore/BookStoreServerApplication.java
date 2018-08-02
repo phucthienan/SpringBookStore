adf0960d8c9bffe08dd4e471b20485c7a6e3c506
Nguyen Phuc Thien An <npta97@gmail.com>
2018-08-02 09:54:56 +0700

    Init book-store-server Spring project

---
+++ book-store-server/src/main/java/com/nguyenphucthienan/bookstore/BookStoreServerApplication.java
@@ -0,0 +1,12 @@
+package com.nguyenphucthienan.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreServerApplication.class, args);
+	}
+}
